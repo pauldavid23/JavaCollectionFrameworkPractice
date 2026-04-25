fbd6ce933e38cdf9b008fa9bdf99e455d05af36e
pauldavid23 <paul.david.gelario@gmail.com>
2026-04-26 02:22:53 +0800

    Adding new codes

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> i = new ArrayList<>(10);
-        ArrayList<Integer> j = new ArrayList<>(List.of(10,20,30,40));
+        LinkedList<Integer> i = new LinkedList<>();
+        LinkedList<Integer> j = new LinkedList<>(List.of(10,20,30,40));
 
-        i.add(10);
+        //i.add(10);
         i.add(0,1);
         i.add(1,2);
+        i.addAll(j);
+
+        i.addFirst(0);
+        i.addLast(50);
 
 
         System.out.println(i);
         System.out.println(j);
+
 
 
     }
